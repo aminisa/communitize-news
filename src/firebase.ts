68832951783e14731e1358163273ae815106ca63
Shadan Amini <shadanamini@Shadans-MacBook-Pro.local>
2024-10-19 17:11:43 -0700

    chore: removed unused import

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,7 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: "AIzaSyADmfjS2_CYkXCeNHsS5qzjRuGxo1lSKDI",
